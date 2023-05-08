27b7122e37a53728d873d64696214d9695bc372e
amberlchen <132846967+amberlchen@users.noreply.github.com>
2023-05-08 14:50:06 -0400

    css and prettier changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,28 @@
-
 export default function About() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between">
-      hi i'm amber! When I'm not behind the camera, i'm travelling, surfing.
-     </main>
-    )
-  }
-  
+  return (
+    <main className="flex min-h-screen flex-col items-center space-y-8">
+      <img
+        alt="A photo of Amber in northern Vietnam"
+        src="images/DSC_0839.jpg"
+      />
+
+      <div className="columns-1 gap-8  sm:columns-2 text-xs text-justify leading-7">
+        <p>
+          I grew up in the United States, raised in Maryland, and after
+          university, I moved to New York City for 6 years. Last year I quit my
+          9-5 job as a frontend engineer to travel the world and cure my
+          restlessness,but I ended up falling in love with Bali, and stayed
+          there for 8 out of the 11 months of my trip. I fell in love with
+          surfing there as well as being a 6 minute walk from the ocean. My
+          parents originally emigrated from Taiwan, and that is where I got my
+          love of food from, and then my love of photography from my father. I
+          used to secretly save up to buy cameras and then hide them from my
+          parents because I didn’t want them to find out - that I do have a gear
+          obsession. Photography has always been part of my life whether it’s
+          the childhood years of sleepover selfies and photoshoots, to now
+          traveling and documenting moments and landscapes.
+        </p>
+      </div>
+    </main>
+  );
+}
